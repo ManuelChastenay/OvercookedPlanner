ab9171b14b5ab3c06029ca75eff86c8baae62fb2
JakeComeau <jacob.comeau.1@ulaval.ca>
2023-12-17 23:20:38 -0500

    Chu con

--- OvercookedPlanner/src/main/java/org/example/Main.java
+++ OvercookedPlanner/src/main/java/org/example/Main.java
@@ -5,14 +5,12 @@ import org.example.solver.OvercookedPlannerApp;
 import org.example.utils.GridFileReader;
 import org.example.utils.Pathfinding;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_1/MapGridConfiguration.txt");
-        //Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_2/MapGridConfiguration.txt");
-        //Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_3/MapGridConfiguration.txt");
-        //Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_4/MapGridConfiguration.txt");
+        Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_1/map_1.txt");
+        //Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_2/map_1.txt");
+        //Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_3/map_1.txt");
+        //Grid grid = GridFileReader.getGridfromConfigFile("Overcooked 1_4/map_1.txt");
         Pathfinding.setGrid(grid);
 
         System.out.println(grid);
